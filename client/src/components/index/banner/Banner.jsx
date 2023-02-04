338c3e2e8a493f76d4dac8fe9bf505943509d567
homeroCorsi <hscorsi@hotmail.com>
2023-02-04 01:53:41 -0300

    banner edit

---
+++ client/src/components/index/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Banner.module.css'
+
+const Banner = () => {
+  return (
+    <div className={style.img}>
+        <div className={style.container}/>
+    </div>
+
+  )
+}
+
+export default Banner
