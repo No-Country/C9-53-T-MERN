5f57bdf492b0d293b0bcd1b2178fc3ab3d2f4c29
homeroCorsi <hscorsi@hotmail.com>
2023-02-05 18:40:03 -0300

    banner edits

---
+++ client/src/components/buttons/YellowButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './YellowButton.module.css'
+
+const YellowButton = ({mensaje}) => {
+  return (
+    <div className={style.yellowButton}>iniciar sesion</div>
+  )
+}
+
+export default YellowButton
---
+++ client/src/components/index/banner/banner cards/BannerCard.jsx
@@ -0,0 +1,10 @@
+import style from './BannerCard.module.css'
+
+const BannerCard = () => {
+    return (<>
+        <div className={style.bannerCard}/>
+        
+    </>)
+}
+
+export default BannerCard
