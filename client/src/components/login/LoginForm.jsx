fe747ae1165c104133f8ef644847109cbc4073b9
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-02-23 09:46:20 -0600

    prueba centralizar 1

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -16,76 +16,79 @@ const LoginForm = () => {
   const { userForm, handleChange, handleSubmit } = useForm(AuthContext);
 
   return (
-    <div className={style.loginContainer}>
+    <div className={style.background}>
+      <div className={style.loginContainer}>
 
-      <div className={style.loginForm}>
+        <div className={style.loginForm}>
 
-        <h1 className={style.title}>Iniciar sesión</h1>
+          <h1 className={style.title}>Iniciar sesión</h1>
 
-        <div className={style.container}>
+          <div className={style.container}>
 
-          <form
-            onSubmit={handleSubmit}
-          >
+            <form
+              onSubmit={handleSubmit}
+            >
 
-            <div className={style.logCategory}>
+              <div className={style.logCategory}>
 
-              <div className={style.logCategoryType}>Dirección de e-mail</div>
+                <div className={style.logCategoryType}>Dirección de e-mail</div>
 
-              <input
-                placeholder="Ingresa una dirección de email"
-                type="text"
-                name="email"
-                className={style.login_input}
-                value={userForm.email}
-                onChange={handleChange}
-              />
+                <input
+                  placeholder="Ingresa una dirección de email"
+                  type="text"
+                  name="email"
+                  className={style.login_input}
+                  value={userForm.email}
+                  onChange={handleChange}
+                />
 
-            </div>
+              </div>
 
-            <div className={style.logCategory}>
+              <div className={style.logCategory}>
 
-              <div className={style.logCategoryType}>Contraseña:</div>
-              <input
-                placeholder="Ingresa una contraseña"
-                type="password"
-                name="password"
-                className={style.login_input2}
-                onChange={handleChange}
-                value={userForm.password}
-              />
+                <div className={style.logCategoryType}>Contraseña:</div>
+                <input
+                  placeholder="Ingresa una contraseña"
+                  type="password"
+                  name="password"
+                  className={style.login_input2}
+                  onChange={handleChange}
+                  value={userForm.password}
+                />
 
-            </div>
+              </div>
 
-            <div className={style.logCategory}>
+              <div className={style.logCategory}>
 
-              <div className={style.logCategoryType2}>¿Olvidaste tu contraseña?</div>
+                <div className={style.logCategoryType2}>¿Olvidaste tu contraseña?</div>
 
-            </div>
+              </div>
 
-            <button type="submit" className={style.blackButton}>
-              Iniciar sesión
+              <button type="submit" className={style.blackButton}>
+                Iniciar sesión
+              </button>
+
+            </form>
+
+            <button className={style.blackButtonCrearCuenta} onClick={register}>
+              Crear cuenta
             </button>
 
-          </form>
+            <div className={style.subtitle}>O ingresa con</div>
 
-          <button className={style.blackButtonCrearCuenta} onClick={register}>
-            Crear cuenta
-          </button>
+            <div className={style.containerLogos}>
+              <div className={style.google}></div>
+              <div className={style.facebook}></div>
+            </div>
 
-          <div className={style.subtitle}>O ingresa con</div>
-
-          <div className={style.containerLogos}>
-            <div className={style.google}></div>
-            <div className={style.facebook}></div>
           </div>
 
+
+
         </div>
-
-
-
       </div>
     </div>
+
   )
 }
 
