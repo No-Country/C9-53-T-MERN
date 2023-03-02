e886ced17f63c5bc49fadaa973d82061317bf621
homeroCorsi <hscorsi@hotmail.com>
2023-03-02 04:27:26 -0300

    layout fixes (2/3)

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,24 +1,11 @@
 import React from "react";
-import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext";
-import { EjsByDayContext } from "../../context/ejsByDay/EjsByDayContext";
-import User from "../../pages/User";
-import YellowButton from "../buttons/YellowButton";
 import ConditionalNavBar from "./conditionalNav/ConditionalNavBar";
-import LogNavBar from "./conditionalNav/LogNavBar";
-import UserNavBar from "./conditionalNav/UserNavBar";
 import style from "./NavBar.module.css";
 
 const NavBar = () => {
 
-  const {logout} = useContext(AuthContext);
 
-  // const {rutineMon} = useContext(EjsByDayContext)
-
-  // const logger= () =>{
-  //     console.log(rutineMon[0].item)
-  // }
 
   return (
     <div className={style.NavBar}>
