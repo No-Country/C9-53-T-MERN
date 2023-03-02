286b13313473e9368dcbca7419b82007006a3553
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2023-03-02 05:50:25 -0600

    cambio de url en cors

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://c9-53-fithouse-tkgv.vercel.app/',
     credentials: true,
   })
 );
