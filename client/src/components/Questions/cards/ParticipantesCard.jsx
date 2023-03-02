b77ef1b7772699fd3aadec0766d20d848f92f826
homeroCorsi <hscorsi@hotmail.com>
2023-03-02 19:53:41 -0300

    merge backups

--- client/src/components/Questions/cards/ParticipantesCard.jsx
+++ client/src/components/Questions/cards/ParticipantesCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import style from './participantesCards.module.css'
+import HomeroSalvadorCorsi from './HomeroSalvadorCorsi.jpg'
 
 const ParticipantesCard = () => {
   return (<>
  <div className={style.card}>
-  <img src="img_avatar.png" alt="Avatar" style={{width:"100%", backgroundColor:'white'}}/>
+  <img src={HomeroSalvadorCorsi} alt="Avatar" style={{width:"100%", backgroundColor:'white'}}/>
   <div className={style.container}>
-    <h4><b>homero Corsi</b></h4>
-    <p>Architect & Engineer</p>
+    <h4><b>Homero Salvador Corsi</b></h4>
+    <p>Frontend</p>
   </div>
 </div> 
 </>
